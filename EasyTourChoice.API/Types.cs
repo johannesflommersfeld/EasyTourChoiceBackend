9901f1e12836f1e6405e12b5f81cd417ecff53b1
johannesflommersfeld <johannesflommersfeld@users.noreply.github.com>
2024-08-17 07:59:43 +0200

    added more climbing grades and avalanche risk

--- EasyTourChoice.API/Types.cs
+++ EasyTourChoice.API/Types.cs
@@ -10,10 +10,13 @@ public enum Activity
     MULTIPITCHCLIMBING,
     VIA_VERRATA,
     MOUNTAINBIKING,
+    ROADCYCLING,
+    GRAVEL,
+    BIKEPACKING,
     SKITOURING,
 }
 
-public enum RiskLevels
+public enum RiskLevel
 {
     UNKNOWN,
     VERY_SAFE,
@@ -21,6 +24,15 @@ public enum RiskLevels
     MODERATE_RISK,
     HIGH_RISK,
     DANGEROUS,
+}
+
+public enum AvelanchRisk
+{
+    LOW,
+    MODERATE,
+    CONSIDERABLE,
+    HIGH,
+    VERY_HIGH,
 }
 
 public enum GeneralDifficulty
@@ -32,7 +44,7 @@ public enum GeneralDifficulty
     VERY_CHALLENING
 }
 
-public enum BoudlerDifficultyUS
+public enum VScale
 {
     UNKNOWN,
     V1,
@@ -51,4 +63,139 @@ public enum BoudlerDifficultyUS
     V15,
     V16,
     V17,
+}
+
+public enum FontembleauScale
+{
+    unknown,
+    FOUR,
+    FIVE,
+    FIVE_PLUS,
+    SIX_A,
+    SIX_A_PLUS,
+    SIX_B,
+    SIX_B_PLUS,
+    SIX_C,
+    SIX_C_PLUS,
+    SEVEN_A,
+    SEVEN_A_PLUS,
+    SEVEN_B,
+    SEVEN_B_PLUS,
+    SEVEN_C,
+    SEVEN_C_PLUS,
+    EIGHT_A,
+    EIGHT_A_PLUS,
+    EIGHT_B,
+    EIGHT_B_PLUS,
+    EIGHT_C,
+    EIGHT_C_PLUS,
+    NINE_A,
+}
+
+public enum UIAAScale
+{
+    UNKNOWN,
+    I,
+    II,
+    III,
+    III_PLUS,
+    IV_MINUS,
+    IV,
+    IV_PLUS,
+    V_MINUS,
+    V,
+    VI_PLUS,
+    VI_MINUS,
+    VI,
+    VII_PLUS,
+    VII_MINUS,
+    VII,
+    VIII_PLUS,
+    VIII_MINUS,
+    VIII,
+    V_PLUS,
+    IX_MINUS,
+    IX,
+    IX_PLUS,
+    X_MINUS,
+    X,
+    X_PLUS,
+    XI_MINUS,
+    XI,
+    XI_PLUS,
+    XII_MINUS,
+    XII,
+    XII_PLUS,
+}
+
+public enum FrenchScale
+{
+    UNKNOWN,
+    ONE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE_A,
+    FIVE_B,
+    FIVE_C,
+    SIX_A,
+    SIX_A_PLUS,
+    SIX_B,
+    SIX_B_PLUS,
+    SIX_C,
+    SIX_C_PLUS,
+    SEVEN_A,
+    SEVEN_A_PLUS,
+    SEVEN_B,
+    SEVEN_B_PLUS,
+    SEVEN_C,
+    SEVEN_C_PLUS,
+    EIGHT_A,
+    EIGHT_A_PLUS,
+    EIGHT_B,
+    EIGHT_B_PLUS,
+    EIGHT_C,
+    EIGHT_C_PLUS,
+    NINE_A,
+    NINE_A_PLUS,
+    NINE_B,
+    NINE_B_PLUS,
+    NINE_C,
+}
+
+public enum YosemityScale
+{
+    UNKNOWN,
+    FIVE_TWO,
+    FIVE_THREE,
+    FIVE_FOUR,
+    FIVE_FIVE,
+    FIVE_SIX,
+    FIVE_SEVEN,
+    FIVE_EIGHT,
+    FIVE_NINE,
+    FIVE_TEN_A,
+    FIVE_TEN_B,
+    FIVE_TEN_C,
+    FIVE_TEN_D,
+    FIVE_ELEVEN_A,
+    FIVE_ELEVEN_B,
+    FIVE_ELEVEN_C,
+    FIVE_ELEVEN_D,
+    FIVE_TWELVE_A,
+    FIVE_TWELVE_B,
+    FIVE_TWELVE_C,
+    FIVE_TWELVE_D,
+    FIVE_THIRTEEN_A,
+    FIVE_THIRTEEN_B,
+    FIVE_THIRTEEN_C,
+    FIVE_THIRTEEN_D,
+    FIVE_FOURTEEN_A,
+    FIVE_FOURTEEN_B,
+    FIVE_FOURTEEN_C,
+    FIVE_FOURTEEN_D,
+    FIVE_FIFTEEN_A,
+    FIVE_FIFTEEN_B,
+    FIVE_FIFTEEN_C,
+    FIVE_FIFTEEN_D,
 }
