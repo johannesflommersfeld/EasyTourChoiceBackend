2a8a64280868687e969fc16f2d9cd1c6b3e98e81
johannesflommersfeld <johannesflommersfeld@users.noreply.github.com>
2025-02-02 12:08:33 +0100

    support additional avalanche regions

--- EasyTourChoice.API/Application/DataAggregation/EAWSReportService.cs
+++ EasyTourChoice.API/Application/DataAggregation/EAWSReportService.cs
@@ -41,39 +41,51 @@ public class EAWSReportService(
         return _mapper.Map<AvalancheReportDto>(report);
     }
 
-    private async Task FetchLatestAvalancheReportAsync(string language = "de")
+    private async Task FetchLatestAvalancheReportAsync(string language = "en")
     {
-        await using Stream stream = await _httpService.PerformGetRequestAsync(GetURL(language));
-        using var reader = new JsonTextReader(new StreamReader(stream));
-        try
+        foreach (var url in GetURLs(language))
         {
-            var serializer = new JsonSerializer();
-            var bulletins = (serializer.Deserialize<EAWSReportDto>(reader)?.Bulletins)
-                ?? throw new JsonException("Could not deserialize the latest avalanche report.");
-
-            foreach (var bulletin in bulletins)
+            await using Stream stream = await _httpService.PerformGetRequestAsync(url);
+            using var reader = new JsonTextReader(new StreamReader(stream));
+            try
             {
-                foreach (var region in bulletin.Regions)
+                var serializer = new JsonSerializer();
+                var bulletins = (serializer.Deserialize<EAWSReportDto>(reader)?.Bulletins)
+                    ?? throw new JsonException("Could not deserialize the latest avalanche report.");
+
+                foreach (var bulletin in bulletins)
                 {
-                    _reportsRepository.SaveReport(region.RegionID, _mapper.Map<AvalancheReport>(bulletin));
+                    foreach (var region in bulletin.Regions)
+                    {
+                        _reportsRepository.SaveReport(region.RegionID, _mapper.Map<AvalancheReport>(bulletin));
+                    }
                 }
             }
+            catch (HttpRequestException e)
+            {
+                _logger.LogError("{Message}", e.Message);
+            }
+            catch (JsonException e)
+            {
+                _logger.LogError("{Message}", e.Message);
+            }
         }
-        catch (HttpRequestException e)
-        {
-            _logger.LogError("{Message}", e.Message);
-        }
-        catch (JsonException e)
-        {
-            _logger.LogError("{Message}", e.Message);
-        }
+
     }
 
-    private string GetURL(string language)
+    private string[] GetURLs(string language)
     {
-        // TODO: move to configuration file
-        // Taken from https://avalanche.report/more/open-data
-        //TODO: also get reports from: https://static.avalanche.report/eaws_bulletins/YYYY-MM-DD/...
-        return $"https://static.avalanche.report/bulletins/latest/EUREGIO_{language}_CAAMLv6.json";
+        string datePattern = "yyyy-MM-dd";
+        var date = DateTime.Now.ToString(datePattern);
+        return [
+            $"https://static.avalanche.report/bulletins/latest/EUREGIO_{language}_CAAMLv6.json",
+            $"https://static.avalanche.report/eaws_bulletins/{date}/{date}-AT-02.json",
+            $"https://static.avalanche.report/eaws_bulletins/{date}/{date}-AT-03.json",
+            $"https://static.avalanche.report/eaws_bulletins/{date}/{date}-AT-04.json",
+            $"https://static.avalanche.report/eaws_bulletins/{date}/{date}-AT-05.json",
+            $"https://static.avalanche.report/eaws_bulletins/{date}/{date}-AT-06.json",
+            $"https://static.avalanche.report/eaws_bulletins/{date}/{date}-AT-08.json",
+            $"https://static.avalanche.report/eaws_bulletins/{date}/{date}-DE-BY.json",
+            ];
     }
 }
